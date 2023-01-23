bed93b3385e4078cd8dcb201437bd2d8036b91ca
tantoxx <marnokristanto@gmail.com>
2023-01-23 10:21:05 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+    <!doctype html>
+    <html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>SIAD UKM ALFA OMEGA</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
+
+    <body>
+
+        <style type="text/css">
+
+            .navbar-siad{
+                background: #301E67;
+                border-radius: 0px;
+                border: 1px solid #301E67;
+                margin: 0px;
+                padding: 5px 0px;
+            }
+
+            .navbar-inverse .navbar-brand {
+
+                color: #fff;
+
+            }
+
+            .navbar-inverse .navbar-nav > li > a {
+
+                color: #fff;
+
+            }
+            .navbar-siad > li > a {
+
+                color: #9d9d9d !important;
+
+            }
+
+            .banner{
+                background: #fff;
+                border-radius: 0px;
+                border: 1px solid white;
+                padding: 50px;
+            }
+
+            .banner{
+                color: black;
+            }
+
+            .banner a{
+                padding: 15px 25px;
+                color: black;
+                border: 1px solid black;
+                transition: all 0.5s;
+                margin-right: 10px;
+            }
+
+            .banner a:hover{
+                text-decoration: none;
+                border: 1px dashed white;
+            }
+
+            .banner p{
+                font-size: 13pt;
+            }
+        </style>
+
+
+
+        <nav class="navbar navbar-inverse navbar-siad">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">SIAD UKM ALFA OMEGA 2023</a>
+                </div>
+
+                   
+                </div>
+            </div>
+        </nav>
+
+        <center>
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <div style="margin-top: 140px;">
+                            <h1>Sistem Informasi Arsip Digital</h1>
+                            <h3>UKM Alfa Omega Universitas Musamus</h3>
+
+                            <br>
+                            <br>
+
+                            <a href="user_login.php">LOGIN ANGGOTA</a>
+                            <a href="login.php">LOGIN ADMIN / PENGURUS</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </center>
+
+
+
+
+
+        <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
